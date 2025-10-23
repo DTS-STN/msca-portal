e34a27770239410f01fcbababa14c3d53a2bf334
lei ye <lei.ye@hrsdc-rhdcc.gc.ca>
2025-10-23 10:33:50 -0400

    add inbox button into banner

--- frontend/app/components/app-bar.tsx
+++ frontend/app/components/app-bar.tsx
@@ -1,6 +1,6 @@
 import type { JSX } from 'react';
 
-import { faChevronDown, faRightFromBracket, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faChevronDown, faEnvelope, faRightFromBracket, faUser } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useTranslation } from 'react-i18next';
 
@@ -9,6 +9,7 @@ import { AppLink } from '~/components/links';
 import { MenuItem } from '~/components/menu';
 import { useLanguage } from '~/hooks/use-language';
 import { cn } from '~/utils/tailwind-utils';
+import { ButtonLink } from './button-link';
 
 type AppBarProps = {
   name?: string;
@@ -21,14 +22,30 @@ export function AppBar({ name }: AppBarProps): JSX.Element {
   return (
     <div className="bg-slate-700">
       <div className="align-center container mx-auto flex flex-wrap justify-between">
-        <div className="align-center flex">
+        <div className="align-center flex order-1">
           <span id="menu-label" className="my-auto py-2 text-white sm:text-2xl">
             <AppLink to={t('gcweb:app.menu-dashboard.href', { baseUri: MSCA_BASE_URL })} className="hover:underline">
               {t('gcweb:app.title')}
             </AppLink>
           </span>
         </div>
-        <div className="flex items-center space-x-4 text-right">{name && <UserButton name={name} />}</div>
+        <div className="my-2 ml-auto mr-8 order-3 sm:order-2 w-full sm:w-auto">
+          <ButtonLink
+            id="inbox-button-desktop" 
+            to={t('gcweb:app.menu-dashboard.href')}
+            variant="default"
+            className="text-lg rounded font-body"
+            refPageAA="ESDC-EDSC_MSCA-MDSC-SCH:Nav"
+          >
+            <FontAwesomeIcon
+              icon={faEnvelope}
+              transform="grow-2"
+              className="mr-2"
+            />
+            <span>{t('gcweb:app.inbox')}</span>
+          </ButtonLink>
+        </div>
+        <div className="flex items-center space-x-4 text-right order-2 sm:order-3 w-full sm:w-auto">{name && <UserButton name={name} />}</div>
       </div>
     </div>
   );
@@ -48,7 +65,7 @@ function UserButton({ className, name }: UserButtonProps): JSX.Element {
     <DropdownMenu>
       <DropdownMenuTrigger
         className={cn(
-          'flex h-full flex-nowrap space-x-2 bg-slate-200 px-2 text-sm text-slate-700 hover:bg-neutral-300 hover:underline focus:bg-neutral-300 focus:ring-2 focus:ring-black focus:ring-offset-2 focus:outline-hidden aria-expanded:bg-neutral-300 aria-expanded:text-slate-700 sm:space-x-4 sm:px-4',
+          'flex h-full w-full flex-nowrap space-x-2 bg-slate-200 px-2 text-sm text-slate-700 hover:bg-neutral-300 hover:underline focus:bg-neutral-300 focus:ring-2 focus:ring-black focus:ring-offset-2 focus:outline-hidden aria-expanded:bg-neutral-300 aria-expanded:text-slate-700 sm:space-x-4 sm:px-4',
           className,
         )}
       >
@@ -56,11 +73,11 @@ function UserButton({ className, name }: UserButtonProps): JSX.Element {
           <div className="flex h-8 w-8 items-center justify-center rounded-full bg-slate-700">
             <FontAwesomeIcon icon={faUser} className="size-5 text-slate-200" />
           </div>
-          <span id="menu-label" className="text-md hidden py-2 font-bold sm:block">
+          <span id="menu-label" className="text-md py-2 font-bold">
             {name}
           </span>
         </div>
-        <FontAwesomeIcon icon={faChevronDown} className="my-auto size-4" />
+        <FontAwesomeIcon icon={faChevronDown} className="my-auto size-4 ml-auto" />
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" className="w-64">
         <UserName name={name} />
