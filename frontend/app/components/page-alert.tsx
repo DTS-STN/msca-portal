17c1202eb7e6945afcf1c09c20da07cb048de1b1
abalogh-esdc <adam.balogh@hrsdc-rhdcc.gc.ca>
2025-10-30 16:12:08 -0400

    Add alert component

---
+++ frontend/app/components/page-alert.tsx
@@ -0,0 +1,65 @@
+import type { ReactNode } from 'react';
+
+import { faCheckCircle, faCircleInfo, faExclamationCircle, faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { cn } from '~/utils/tailwind-utils';
+
+export type AlertType = 'warning' | 'success' | 'danger' | 'info' | 'comment';
+
+export interface ContextualAlertProps {
+  children: ReactNode;
+  type: AlertType;
+}
+
+const alertBackgroundColors: Partial<Record<AlertType, string>> & { default: string } = {
+  comment: 'bg-sky-50',
+  default: 'bg-white',
+};
+
+const alertBorderColors: Partial<Record<AlertType, string>> & { default: string } = {
+  comment: 'border-l-sky-800',
+  danger: 'border-l-red-700',
+  default: 'border-l-gray-700',
+  info: 'border-l-cyan-700',
+  warning: 'border-l-amber-700',
+  success: 'border-l-green-700',
+};
+
+export function ContextualAlert(props: ContextualAlertProps) {
+  const { children, type } = props;
+
+  const alertBackgroundColor = alertBackgroundColors[type] ?? alertBackgroundColors.default;
+  const alertBorderColor = alertBorderColors[type] ?? alertBorderColors.default;
+
+  return (
+    <div className={cn('relative pl-4 sm:pl-6', alertBackgroundColor)}>
+      <div className={cn('absolute top-3 left-1.5 pt-1 pb-1 sm:left-3.5', alertBackgroundColor)}>
+        <Icon type={type} />
+      </div>
+      <div className={cn('text-gray-darker font-display overflow-auto border-l-4 pt-3 pb-4.5 pl-6 text-2xl', alertBorderColor)}>
+        {children}
+      </div>
+    </div>
+  );
+}
+
+function Icon({ type }: { type: string }) {
+  switch (type) {
+    case 'warning': {
+      return <FontAwesomeIcon icon={faExclamationTriangle} className="h-6 w-6 text-amber-700" />;
+    }
+    case 'success': {
+      return <FontAwesomeIcon icon={faCheckCircle} className="h-6 w-6 text-green-700" />;
+    }
+    case 'danger': {
+      return <FontAwesomeIcon icon={faExclamationCircle} className="h-6 w-6 text-red-700" />;
+    }
+    case 'info': {
+      return <FontAwesomeIcon icon={faCircleInfo} className="h-6 w-6 text-cyan-700" />;
+    }
+    default: {
+      break;
+    }
+  }
+}
