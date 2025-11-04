b76e632e56cb5c5f7a683ce4a4b3a77ff440ef51
Charles-Pham <charles.pham@hrsdc-rhdcc.gc.ca>
2025-11-04 08:53:32 -0700

    add french ei strings and extract components (#19)

---
+++ frontend/app/components/province-mail-contact-card.tsx
@@ -0,0 +1,43 @@
+type ProvinceMailContactCardProps = {
+  addrs: {
+    summary: string;
+    forReports: {
+      title: string;
+      addr: string[];
+    };
+    forDocs: {
+      title: string;
+      addr: string[];
+    };
+  };
+};
+
+export default function ProvinceMailContactCard({ addrs }: ProvinceMailContactCardProps) {
+  return (
+    <>
+      <div className="py-2">
+        <details className="mb-5px font-body text-20px text-gray-darker">
+          <summary className="border-gray-40 px-15px py-5px text-deep-blue-60d hover:text-blue-hover cursor-pointer rounded border outline-none select-none hover:underline">
+            {addrs.summary}
+          </summary>
+          <div className="border-gray-40 px-18px py-5px cursor-pointer rounded-b border outline-none select-none">
+            <div className="grid grid-cols-2 text-xl">
+              <div className="font-display col-span-2 cursor-default py-3 select-text md:col-span-1">
+                <p className="font-bold">{addrs.forReports.title}</p>
+                {addrs.forReports.addr.map((addrLine, _index) => (
+                  <p>{addrLine}</p>
+                ))}
+              </div>
+              <div className="font-display col-span-2 cursor-default py-3 select-text md:col-span-1">
+                <p className="font-bold">{addrs.forDocs.title}</p>
+                {addrs.forDocs.addr.map((addrLine, _index) => (
+                  <p>{addrLine}</p>
+                ))}
+              </div>
+            </div>
+          </div>
+        </details>
+      </div>
+    </>
+  );
+}
--- frontend/app/routes/contact-us.tsx
+++ frontend/app/routes/contact-us.tsx
@@ -1,59 +1,76 @@
-import { requireAuth } from "~/.server/utils/auth-utils";
-import { Route } from "./+types/contact-us";
-import { getTranslation } from "~/i18n-config.server";
+import { RouteHandle } from 'react-router';
+
+import { useTranslation } from 'react-i18next';
+
+import { Route } from './+types/contact-us';
+
+import { requireAuth } from '~/.server/utils/auth-utils';
+import { InlineLink } from '~/components/links';
+import { PageTitle } from '~/components/page-title';
+import { AppError } from '~/errors/app-error';
+import { ErrorCodes } from '~/errors/error-codes';
+import { getTranslation } from '~/i18n-config.server';
 import { handle as parentHandle } from '~/routes/layout';
-import { RouteHandle } from "react-router";
-import { AppError } from "~/errors/app-error";
-import { ErrorCodes } from "~/errors/error-codes";
-import { useTranslation } from "react-i18next";
-import { PageTitle } from "~/components/page-title";
-import { InlineLink } from "~/components/links";
 
 export const handle = {
-    i18nNamespace: [...parentHandle.i18nNamespace],
+  i18nNamespace: [...parentHandle.i18nNamespace],
 } as const satisfies RouteHandle;
 
 export async function loader({ context, params, request }: Route.LoaderArgs) {
-    const { userinfoTokenClaims } = await requireAuth(context.session, request);
-    const { t } = await getTranslation(request, handle.i18nNamespace);
+  const { userinfoTokenClaims } = await requireAuth(context.session, request);
+  const { t } = await getTranslation(request, handle.i18nNamespace);
 
-    if (!userinfoTokenClaims.sin) {
-        throw new AppError('No SIN found in userinfo token', ErrorCodes.MISSING_SIN);
-    }
+  if (!userinfoTokenClaims.sin) {
+    throw new AppError('No SIN found in userinfo token', ErrorCodes.MISSING_SIN);
+  }
 
-    return { documentTitle: t('contactUs:document-title') };
+  return { documentTitle: t('contactUs:document-title') };
 }
 
 export function meta({ data }: Route.MetaArgs) {
-    return [{ title: data?.documentTitle }];
+  return [{ title: data?.documentTitle }];
 }
 
 export default function ContactUs({ loaderData, params }: Route.ComponentProps) {
+  const { t } = useTranslation(handle.i18nNamespace);
 
-    const { t } = useTranslation(handle.i18nNamespace);
+  return (
+    <>
+      <div className="max-w-3xl">
+        <div className="mb-8">
+          <PageTitle className="after:w-14">{t('contactUs:page-title')}</PageTitle>
+        </div>
 
-    return (
-        <>
-            <div className="max-w-3xl">
-                <div className="mb-8">
-                    <PageTitle className="after:w-14">{t('contactUs:page-title')}</PageTitle>
-                </div>
+        <p className="text-gray-darker mt-3 mb-8 text-xl">{t('contactUs:select-service')}</p>
 
-                <p className="mb-8 mt-3 text-xl text-gray-darker">{t('contactUs:select-service')}</p>
-
-                <ul className="list-disc" data-cy="contact-task-list">
-                    <li className="mb-6 ml-5"><InlineLink to={t('contactUs:cdcp.href')} newTabIndicator={true}>{t('contactUs:cdcp')}</InlineLink></li>
-                    <li className="mb-6 ml-5"><InlineLink file="routes/contact-us/employment-insurance.tsx">{t('contactUs:ei')}</InlineLink></li>
-                    <li className="mb-6 ml-5"><InlineLink file="routes/contact-us/canada-pension-plan.tsx">{t('contactUs:cpp')}</InlineLink></li>
-                    <li className="mb-6 ml-5">
-                        <InlineLink file="routes/contact-us/old-age-security.tsx">{t('contactUs:oas')}</InlineLink>
-                        <p className="text-xl text-gray-darker">{t('contactUs:oas.more-info')}</p>
-                    </li>
-                    <li className="mb-6 ml-5"><InlineLink to={t('contactUs:cdb.href')} newTabIndicator={true}>{t('contactUs:cdb')}</InlineLink></li>
-                    <li className="mb-6 ml-5"><InlineLink to={t('contactUs:sin.href')} newTabIndicator={true}>{t('contactUs:sin')}</InlineLink></li>
-                </ul>
-            </div>
-
-        </>
-    );
+        <ul className="list-disc" data-cy="contact-task-list">
+          <li className="mb-6 ml-5">
+            <InlineLink to={t('contactUs:cdcp.href')} newTabIndicator={true}>
+              {t('contactUs:cdcp')}
+            </InlineLink>
+          </li>
+          <li className="mb-6 ml-5">
+            <InlineLink file="routes/contact-us/employment-insurance.tsx">{t('contactUs:ei')}</InlineLink>
+          </li>
+          <li className="mb-6 ml-5">
+            <InlineLink file="routes/contact-us/canada-pension-plan.tsx">{t('contactUs:cpp')}</InlineLink>
+          </li>
+          <li className="mb-6 ml-5">
+            <InlineLink file="routes/contact-us/old-age-security.tsx">{t('contactUs:oas')}</InlineLink>
+            <p className="text-gray-darker text-xl">{t('contactUs:oas.more-info')}</p>
+          </li>
+          <li className="mb-6 ml-5">
+            <InlineLink to={t('contactUs:cdb.href')} newTabIndicator={true}>
+              {t('contactUs:cdb')}
+            </InlineLink>
+          </li>
+          <li className="mb-6 ml-5">
+            <InlineLink to={t('contactUs:sin.href')} newTabIndicator={true}>
+              {t('contactUs:sin')}
+            </InlineLink>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 }
