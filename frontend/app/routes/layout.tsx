62d135d8912670663140bd4dd2bed01db3dfdc41
Caroline George <caroline.e.george@hrsdc-rhdcc.gc.ca>
2025-11-04 17:05:29 -0500

    temp stash

--- frontend/app/routes/layout.tsx
+++ frontend/app/routes/layout.tsx
@@ -15,11 +15,10 @@ import { SessionTimeout } from '~/components/session-timeout';
 import { SkipNavigationLinks } from '~/components/skip-navigation-links';
 import { useLanguage } from '~/hooks/use-language';
 import { useRoute } from '~/hooks/use-route';
-import InboxNotificationPreferences from './inbox-notification-preferences';
 
 export const handle = {
 
-  i18nNamespace: ['app', 'gcweb', 'inbox', 'inboxNowAvailable', 'inboxNotificationPreferencesSuccess', 'inboxNotificationPreferences', 'contactUs', 'contactUsEi'],
+  i18nNamespace: ['app', 'gcweb', 'inbox', 'inboxNowAvailable', 'inboxNotificationPreferencesSuccess', 'inboxNotificationPreferences', 'profileAndPreferences', 'contactUs', 'contactUsEi'],
 
 } as const satisfies RouteHandle;
 
