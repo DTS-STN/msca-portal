39a68e4acadb0f1cee4ac19ea9a9f4a99f23dde7
lei ye <lei.ye@hrsdc-rhdcc.gc.ca>
2025-11-17 07:59:24 -0500

    update the application bar

--- frontend/app/components/app-bar.tsx
+++ frontend/app/components/app-bar.tsx
@@ -1,12 +1,19 @@
+import { useState } from 'react';
 import type { JSX } from 'react';
 
-import { faChevronDown, faEnvelope, faRightFromBracket, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faChevronUp, faChevronDown, faEnvelope, faRightFromBracket, faUser } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useTranslation } from 'react-i18next';
 
 import { ButtonLink } from './button-link';
 
-import { DropdownMenu, DropdownMenuContent, DropdownMenuLabel, DropdownMenuTrigger } from '~/components/dropdown-menu';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '~/components/dropdown-menu';
 import { AppLink } from '~/components/links';
 import { MenuItem } from '~/components/menu';
 import { useLanguage } from '~/hooks/use-language';
@@ -21,9 +28,9 @@ export function AppBar({ name }: AppBarProps): JSX.Element {
   const { MSCA_BASE_URL } = globalThis.__appEnvironment;
 
   return (
-    <div className="bg-slate-700">
+    <div className="md:bg-slate-700">
       <div className="align-center container mx-auto flex flex-wrap justify-between">
-        <div className="align-center order-1 flex">
+        <div className="align-center order-1 flex w-full bg-slate-700 sm:w-auto">
           <span id="menu-label" className="my-auto py-2 text-white sm:text-2xl">
             <AppLink to={t('gcweb:app.menu-dashboard.href', { baseUri: MSCA_BASE_URL })} className="hover:underline">
               {t('gcweb:app.title')}
@@ -35,15 +42,20 @@ export function AppBar({ name }: AppBarProps): JSX.Element {
             id="inbox-button-desktop"
             to={t('gcweb:app.inbox.href', { baseUri: MSCA_BASE_URL })}
             variant="default"
-            className="font-body rounded text-lg"
+            className="rounded border-2 border-[#2B4380] bg-white font-sans text-lg"
             refPageAA="ESDC-EDSC_MSCA-MDSC-SCH:Nav"
           >
             <FontAwesomeIcon icon={faEnvelope} transform="grow-2" className="mr-2" />
             <span>{t('gcweb:app.inbox')}</span>
           </ButtonLink>
         </div>
-        <div className="order-2 flex w-full items-center space-x-4 text-right sm:order-3 sm:w-auto">
-          {name && <UserButton name={name} />}
+        <div className="ring-blue-hover order-2 flex w-full items-center text-right text-2xl ring-offset-2 focus:ring-2 focus:outline-none sm:order-3 sm:w-[260px]">
+          {name && (
+            <UserButton
+              name={name}
+              className="ring-blue-hover focus:ring-blue-hover active:ring-blue-hover ring-offset-2 focus:ring-2 active:ring-2"
+            />
+          )}
         </div>
       </div>
     </div>
@@ -59,12 +71,13 @@ function UserButton({ className, name }: UserButtonProps): JSX.Element {
   const { t } = useTranslation(['gcweb']);
   const { currentLanguage } = useLanguage();
   const { MSCA_BASE_URL } = globalThis.__appEnvironment;
+  const [open, setOpen] = useState(false);
 
   return (
-    <DropdownMenu>
+    <DropdownMenu open={open} onOpenChange={setOpen}>
       <DropdownMenuTrigger
         className={cn(
-          'flex h-full w-full flex-nowrap space-x-2 bg-slate-200 px-2 text-sm text-slate-700 hover:bg-neutral-300 hover:underline focus:bg-neutral-300 focus:ring-2 focus:ring-black focus:ring-offset-2 focus:outline-hidden aria-expanded:bg-neutral-300 aria-expanded:text-slate-700 sm:space-x-4 sm:px-4',
+          'flex h-full w-full flex-nowrap space-x-2 bg-slate-200 px-2 text-slate-700 hover:bg-neutral-300 focus:bg-neutral-300 focus:ring-2 focus:ring-black focus:ring-offset-2 focus:outline-hidden aria-expanded:bg-neutral-300 aria-expanded:text-slate-700 sm:space-x-4 sm:px-4',
           className,
         )}
       >
@@ -72,42 +85,45 @@ function UserButton({ className, name }: UserButtonProps): JSX.Element {
           <div className="flex h-8 w-8 items-center justify-center rounded-full bg-slate-700">
             <FontAwesomeIcon icon={faUser} className="size-5 text-slate-200" />
           </div>
-          <span id="menu-label" className="text-md py-2 font-bold">
+          <span id="menu-label" className="ring-blue-hover py-2 text-lg font-bold">
             {name}
           </span>
         </div>
-        <FontAwesomeIcon icon={faChevronDown} className="my-auto ml-auto size-4" />
+        <FontAwesomeIcon icon={open ? faChevronUp : faChevronDown} className="my-auto ml-auto size-4" />
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end" className="w-64">
+      <DropdownMenuContent align="start" sideOffset={0} className="w-(--radix-dropdown-menu-trigger-width) focus:outline-none">
         <UserName name={name} />
         <MenuItem
           to={t('gcweb:app.menu-dashboard.href', { baseUri: MSCA_BASE_URL })}
-          className="text-md flex justify-between text-black hover:bg-zinc-100 hover:text-black focus:bg-zinc-100 active:bg-zinc-100"
+          className="text-md text-deep-blue-dark hover:text-blue-hover flex justify-between focus:bg-white"
         >
           {t('gcweb:app.menu-dashboard')}
         </MenuItem>
+        <DropdownMenuSeparator />
         <MenuItem
           to={t('gcweb:app.profile-and-preferences.href', { baseUri: MSCA_BASE_URL })}
-          className="text-md flex justify-between text-black hover:bg-zinc-100 hover:text-black focus:bg-zinc-100 active:bg-zinc-100"
+          className="text-md text-deep-blue-dark hover:text-blue-hover flex justify-between focus:bg-white"
         >
           {t('gcweb:app.profile')}
         </MenuItem>
+        <DropdownMenuSeparator />
         <MenuItem
           to={t('gcweb:app.security-settings.href', { baseUri: MSCA_BASE_URL })}
-          className="text-md flex justify-between text-black hover:bg-zinc-100 hover:text-black focus:bg-zinc-100 active:bg-zinc-100"
+          className="text-md text-deep-blue-dark hover:text-blue-hover flex justify-between focus:bg-white"
         >
           {t('gcweb:app.security-settings')}
         </MenuItem>
+        <DropdownMenuSeparator />
         <MenuItem
           to={t('gcweb:app.contact-us.href', { baseUri: MSCA_BASE_URL })}
-          className="text-md flex justify-between text-black hover:bg-zinc-100 hover:text-black focus:bg-zinc-100 active:bg-zinc-100"
+          className="text-md text-deep-blue-dark hover:text-blue-hover flex justify-between focus:bg-white"
         >
           {t('gcweb:app.contact-us')}
         </MenuItem>
-        <div role="separator" aria-orientation="horizontal" className="-mx-1 my-1 h-px bg-slate-100"></div>
+        <DropdownMenuSeparator />
         <MenuItem
           to={`/auth/logout?lang=${currentLanguage}`}
-          className="text-md flex justify-between text-black hover:bg-zinc-100 hover:text-black focus:bg-zinc-100 active:bg-zinc-100"
+          className="text-md text-deep-blue-dark hover:text-blue-hover flex justify-between focus:bg-white"
         >
           {t('gcweb:app.logout')}
           <FontAwesomeIcon icon={faRightFromBracket} className="my-auto size-8" />
