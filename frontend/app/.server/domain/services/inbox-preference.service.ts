c4443779a17c20e3ed2806528eb4e1fbd3c5500a
lei ye <lei.ye@hrsdc-rhdcc.gc.ca>
2025-11-19 11:56:52 -0500

    update log

--- frontend/app/.server/domain/services/inbox-preference.service.ts
+++ frontend/app/.server/domain/services/inbox-preference.service.ts
@@ -1,6 +1,18 @@
+import moize from 'moize';
+
 import { getInboxPrefRepository } from '../repositories/inbox-preference.repository';
 
-export function getInboxPrefService() {
+import { LogFactory } from '~/.server/logging';
+
+const log = LogFactory.getLogger(import.meta.url);
+
+const { LOOKUP_SVC_DASHBOARD_CACHE_TTL_SECONDS } = globalThis.__appEnvironment;
+
+export const getInboxPrefService = moize(createInboxPrefService, {
+  onCacheAdd: () => log.info('Creating new dashboard service'),
+});
+
+export function createInboxPrefService() {
   const repo = getInboxPrefRepository();
 
   async function getInboxPre(spid: string) {
@@ -16,7 +28,13 @@ export function getInboxPrefService() {
   }
 
   return {
-    getInboxPre,
-    setInboxPref,
+    getInboxPre: moize(getInboxPre, {
+      maxAge: 1000 * LOOKUP_SVC_DASHBOARD_CACHE_TTL_SECONDS,
+      onCacheAdd: () => log.info('Creating new getPageAlertsByLang memo'),
+    }),
+    setInboxPref: moize(setInboxPref, {
+      maxAge: 1000 * LOOKUP_SVC_DASHBOARD_CACHE_TTL_SECONDS,
+      onCacheAdd: () => log.info('Creating new getPageAlertsByLang memo'),
+    }),
   };
 }
