f4f0f4560af0cba2b9b2f2c155faf127373dcdbf
abalogh-esdc <adam.balogh@hrsdc-rhdcc.gc.ca>
2025-11-20 09:55:25 -0500

    Fix button style and text formatting

--- frontend/app/components/button.tsx
+++ frontend/app/components/button.tsx
@@ -20,7 +20,7 @@ const variants = {
   default: 'border-gray-300 bg-gray-200 text-slate-700 hover:bg-neutral-300 focus:bg-neutral-300',
   dark: 'border-gray-800 bg-gray-800 text-white hover:bg-gray-900 focus:bg-gray-900',
   green: 'border-green-700 bg-green-700 text-white hover:bg-green-800 focus:bg-green-800',
-  primary: 'border-slate-700 bg-slate-700 text-white hover:bg-sky-800 focus:bg-sky-800',
+  primary: 'text-white bg-blue-primary hover:bg-deep-blue-focus active:bg-blue-pressed rounded focus:ring focus:ring-offset-4 focus:ring-deep-blue-60f focus:ring-bg-deep-blue-focus',
   red: 'border-red-700 bg-red-700 text-white hover:bg-red-800 focus:bg-red-800',
   link: 'border-0 bg-white text-gray-900 underline hover:bg-gray-100 hover:text-blue-700 focus:bg-gray-100 focus:text-blue-700',
   ghost: 'border-0 hover:bg-neutral-100 hover:text-neutral-900',
